72d62b5e11be4311849973898c1b5840d939396e
Maciej Kachniarz <maciej.kachniarz@predica.pl>
2018-12-09 15:11:27 +0100

    Add pug to formdata

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,9 @@
 exports.store = (req, res) => {
-    res.json({
-        'name' : req.body.name,
-        'last name' : req.body.lastName,
-        'email' : req.body.email
-    });
+    // res.json({
+    //     'name' : req.body.name,
+    //     'last name' : req.body.lastName,
+    //     'email' : req.body.email
+    // });
+
+    res.render('formdata', {name: req.body.name, lastname: req.body.lastName, mail: req.body.email});
 };
